20dff1b03b0645904b748bb7c813bbccb7315b6c
Remington Orange <remingtonorange@yahoo.com>
2024-11-08 00:09:36 -0500

    Fixed

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,4 @@
 module.exports = function (eleventyConfig) {
-    // Add passthrough copies
     eleventyConfig.addPassthroughCopy("_src/_includes/");
     eleventyConfig.addPassthroughCopy("./_src/css/");
 
